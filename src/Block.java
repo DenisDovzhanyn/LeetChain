6113614d44b6f9da7eae5a3170abc45bf2b3fd53
Denis <dovzhanyndenis03@gmail.com>
2024-07-17 01:17:07 -0400

    now checks validity of chain

--- src/Block.java
+++ src/Block.java
@@ -7,6 +7,7 @@ public class Block {
     private long timeStamp;
     private String question;
     private String answer;
+    private int nonce;
 
 
     public Block(String previousHash, String answer, String question){
@@ -18,9 +19,19 @@ public class Block {
     }
 
     public String calHash(){
-        String calculatedHash = Hasher.hash(previousHash + Long.toString(timeStamp) + question + answer);
+        String calculatedHash = Hasher.hash(previousHash + Long.toString(timeStamp) + question + answer + Integer.toString(nonce));
 
         return calculatedHash;
+    }
+
+    public void mineBlock(int difficulty){
+        String targetHash = new String(new char[difficulty]).replace('\0', '0');
+        while(!hash.substring(0,difficulty).equals(targetHash)){
+            nonce++;
+            hash = calHash();
+        }
+
+        System.out.println("Nice you've mined a block");
     }
 
 
--- src/LeetChain.java
+++ src/LeetChain.java
@@ -6,13 +6,35 @@ public class LeetChain {
 
     public static void main(String[] args){
         chain.add(new Block("0","HELP ME IM GOING TO DIE","for int i = 0"));
+        chain.get(0).mineBlock(5);
+
         chain.add(new Block(chain.get(chain.size()-1).hash, "LOLOL", "hmmm idk"));
-        System.out.println("Hash for block 1: " + chain.get(0).hash);
-        System.out.println("Hash for block 2: " + chain.get(1).hash);
-        if(!chain.get(0).hash.equals(chain.get(1).previousHash) || !chain.get(1).hash.equals(chain.get(1).calHash())){
-            System.out.println("Oh hell no! NO tampering");
-        } else {
-            System.out.println("Thanks for not tampering!");
+        chain.get(1).mineBlock(6);
+
+        System.out.println(isChainValid());
+    }
+
+    public static boolean isChainValid(){
+        Block previous;
+        Block current;
+
+        for(int i = 1; i < chain.size(); i++){
+
+            current = chain.get(i);
+            previous = chain.get(i -1);
+
+            if(!previous.hash.equals(current.previousHash)){
+                System.out.println("previous hash tampered");
+                return false;
+            }
+
+            if(!current.hash.equals(current.calHash())){
+                System.out.println("current hash tampered");
+            }
+
+
+
         }
+        return true;
     }
 }
