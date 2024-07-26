0d1d6966b58500fb72b031ca0055b00af90f7522
Denis <dovzhanyndenis03@gmail.com>
2024-07-26 17:11:49 -0400

    feat! : now able to create and verify signatures, transactions will soon be stored on block

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -1,19 +1,18 @@
 import org.bouncycastle.util.encoders.Base32;
 
-import java.security.Key;
-import java.security.MessageDigest;
+import java.security.*;
 
 public class Util {
 
     // takes in input and converts it into a unique hash
-    public static String hash(String input){
+    public static String hash(String input) {
 
         try {
             MessageDigest encrypter = MessageDigest.getInstance("SHA-256");
             byte[] hash = encrypter.digest(input.getBytes("UTF-8"));
             StringBuffer hexHash = new StringBuffer();
 
-            for(int i = 0; i < hash.length; i++){
+            for (int i = 0; i < hash.length; i++) {
                 String hex = Integer.toHexString(0xff & hash[i]);
 
                 if(hex.length() == 1) hexHash.append('0');
@@ -21,16 +20,47 @@ public class Util {
             }
 
             return hexHash.toString();
-
         } catch(Exception f){
             throw new RuntimeException(f);
         }
     }
 
 
-    public static String keyToString(Key key){
+    public static String keyToString(Key key) {
         byte[] publicEncoded = key.getEncoded();
 
         return Base32.toBase32String(publicEncoded);
+    }
+
+    public static byte[] applySignature(PrivateKey privateKey, String input) {
+
+        byte[] output;
+
+        try {
+            Signature signature = Signature.getInstance("ECDSA", "BC");
+            signature.initSign(privateKey);
+
+            byte[] inputBytes = input.getBytes();
+            signature.update(inputBytes);
+
+            output = signature.sign();
+
+        } catch (Exception e) {
+            throw new RuntimeException("error applying signature", e);
+        }
+
+        return output;
+    }
+
+    public static boolean verifySignature(PublicKey publicKey, String data, byte[] signature){
+        try {
+            Signature verify = Signature.getInstance("ECDSA", "BC");
+            verify.initVerify(publicKey);
+            verify.update(data.getBytes());
+
+            return verify.verify(signature);
+        } catch (Exception e) {
+            throw new RuntimeException("error verifying", e);
+        }
     }
 }
