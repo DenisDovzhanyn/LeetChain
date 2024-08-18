cd0093e2aadb91b2b50ddea9678535928661a49d
Denis <dovzhanyndenis03@gmail.com>
2024-08-17 22:58:50 -0400

    feat: added peer class to store and check ip/port/score which will help with reading peer file/ connecting upon startup

---
+++ src/main/java/Node/Peer.java
@@ -0,0 +1,41 @@
+package Node;
+
+public class Peer {
+    String ip;
+    int port;
+    int score;
+
+    public Peer (String ip, int port, int score) {
+        this.ip = ip;
+        this.port = port;
+        this.score = score;
+    }
+
+    public void raiseScoreByOne() {
+        score++;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
--- src/main/java/Node/SocketHandler.java
+++ src/main/java/Node/SocketHandler.java
@@ -5,9 +5,14 @@ import Node.RequestTypes.BlockListRequest;
 import Node.RequestTypes.PeerListRequest;
 import Wallet.Transaction;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -18,24 +23,25 @@ public class SocketHandler implements Runnable{
     ConcurrentLinkedQueue<Block> blocksToOtherNodes;
     ConcurrentLinkedQueue<Transaction> transactionsToOtherNodes;
     ConcurrentLinkedQueue<Block> incomingBlock;
+    List<Peer> peers;
 
     public SocketHandler(ConcurrentLinkedQueue<Block> blocksToOtherNodes, ConcurrentLinkedQueue<Block> incomingBlock) {
         this.blocksToOtherNodes = blocksToOtherNodes;
         this.incomingBlock = incomingBlock;
+        peers = new ArrayList<>();
     }
 
     @Override
     public void run() {
         sockets = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
 
+
+        // we need to try to connect here first with a regular socket before opening up our server for connections from other people
         while (sockets.getPoolSize() <= 9) {
             try {
                 server = new ServerSocket(6478);
                 Socket socket = server.accept();
-                // after we connect we want to raise their score on our peer list file ( we will want to connect to the people with the highest scores when starting program )
-                // also I just realized all these connections will only be people trying to connect with us?
-                // what about when WE want to connect with people ?
-                // maybe in this class first we try to connect with people then after that we open up our server and let people connect to us?
+                checkSocketForPreviousConnection(socket.getRemoteSocketAddress().toString());
 
                 ConcurrentLinkedQueue<BlockListRequest> blockRequests = new ConcurrentLinkedQueue<>();
                 ConcurrentLinkedQueue<PeerListRequest> peerRequests = new ConcurrentLinkedQueue<>();
@@ -55,4 +61,57 @@ public class SocketHandler implements Runnable{
     public int amountOfConnectedSockets() {
         return sockets.getPoolSize() / 2;
     }
+
+    //
+    public List<Peer> peerFileToList() {
+        File peerlist = new File("Peers");
+        List<Peer> peers = new ArrayList<>();
+
+        try {
+            peerlist.createNewFile();
+            Scanner scanner = new Scanner(peerlist);
+
+            while (scanner.hasNext()) {
+                String[] splitLine = scanner.nextLine().split(":");
+                //                   ip            port                           score
+                Peer peer = new Peer(splitLine[0], Integer.parseInt(splitLine[1]), Integer.parseInt(splitLine[2]));
+                peers.add(peer);
+            }
+            scanner.close();
+        } catch (IOException e) {
+            System.out.println("Peers file not found, please create one or confirm correct location");
+        }
+
+        return peers;
+    }
+
+    // compare ip and port to previously connected sockets, if it is a match we raise score by one. if not we set score 1 and add to list
+    public void checkSocketForPreviousConnection(String address) {
+        String[] peerVariables = address.split(":");
+
+        Peer peer = new Peer(peerVariables[0], Integer.parseInt(peerVariables[1]), 1);
+        for (Peer x : peers){
+            if (x.port == peer.port && x.ip.equals(peer.ip)) {
+                x.raiseScoreByOne();
+            }
+        }
+        peers.add(peer);
+    }
+
+    public void writePeersToFile() {
+        try {
+            File file = new File("Peers");
+            file.createNewFile();
+            FileWriter fileWriter = new FileWriter(file);
+
+            for (Peer x : peers) {
+                fileWriter.write(x.getIp() + ":" + x.getPort() + ":" + x.getScore());
+            }
+            fileWriter.close();
+        } catch (IOException e) {
+            System.out.println("Error writing peers to file");
+        }
+    }
+
+
 }
